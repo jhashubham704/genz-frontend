ca90a7e7dab80957f2546f82f505a10b05e87c21
Shubham Jha <jhashubham704@gmail.com>
2023-02-22 18:35:17 +0530

    font update

--- src/screens/about/About.js
+++ src/screens/about/About.js
@@ -1,28 +1,30 @@
+import { Diversity1 } from '@mui/icons-material'
 import React from 'react'
 import './about.css'
 import img from './about.jpg'
 export default function About() {
   return (
     <div className='div-main'>
+      <div className='container'> 
         <div className="div-cont">
-            <p>
+            <p className='heading-block'>
+             <h3 className='heading'>About Us </h3> 
+              </p>
+            <p className='content'>
          Chat and share your feelings with random people like you.
-         <br />
-         <br />
+         
          Read stories of how people come out of anxiety and depression without any medication.
-         <br />
-         <br />
+         
          Shop products/books that help you rewire your brain.
-         <br />
-         <br />
+         
          Help other people like you and earn coins.
-         <br />
-         <br />
+         
          Get guidance and help form expert psychologists, spiritual guru and meditators without any hustle.
             </p>
         </div>
         <div className="div-img">
             <img src={img} alt="" />
+        </div>
         </div>
     </div>
   )
