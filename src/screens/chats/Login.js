9271062a7b65b45e1b5ae70152e3066fc36e0be1
Shubham Jha <jhashubham704@gmail.com>
2023-03-31 18:08:46 +0530

    login and registration created

--- src/screens/chats/Login.js
+++ src/screens/chats/Login.js
@@ -2,62 +2,64 @@ import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import './Login.css'
 import Footer from '../../common/Footer/Footer'
+import './Register'
+import Register from './Register';
+import Alert from '@mui/material/Alert';
+
+
 export default function Login(props) {
 
-    const userData = [
-        {
-          "user" : "admin", 
-          "password": "password"
-        }
-    ]
-    const[user, setUser] = useState ( { 
-        "name":"", 
-        "pass": ""
-     }
-     )
-     const[tempname , setTempname] = useState ("") ; 
-     const[temppass , setTempass] = useState ("") ;
-     const[tempemail , setTempemail] = useState ("") 
-    const[tempnumber , setTempnumber] = useState ("") 
-     const[value, setValue] = useState("true")
-    const[data , useData] = useState()
+  const [temppass, setTemppass] = useState("");
+  const [tempemail, setTempemail] = useState("")
+  const [signup, setsignup] = useState("false")
+  const [value, setValue] = useState("")
+  const [data, useData] = useState()
 
-     const setLogindata=async ()=> { 
-       setUser({ 
-        "name":{tempname} ,
-        "pass": {temppass} ,
-       })
-       props.loginhandler(value) ; 
-      
-
-      let result = await fetch('http://localhost:5001/register' , { 
-        method:'Post',
-        body:JSON.stringify({tempname,tempemail,temppass,tempnumber}) ,
-        headers:{ 
-             'Content-type':'Application/json'
-        }, 
-      }) ; 
-      result = await result.json() ; 
-      console.log(result) ; 
-     }
-
+  const setLogindata = async () => {
+    let result = await fetch('http://localhost:5001/login', {
+      method: 'Post',
+      body: JSON.stringify({ email: tempemail, password: temppass }),
+      headers: {
+        'Content-type': 'Application/json'
+      },
+    });
+    result = await result.json();
+    if (result.password === temppass && result.email === tempemail) {
+      setValue("true");
+    }
+    else {
+      setValue("false");
+    }
+    props.loginhandler(value);
+    console.log(value)
+  }
+  const setalert = () => {
+    if (value === "false") {
+      return (<Alert severity="error">This is an error alert — check it out!</Alert>)
+    }
+    else {
+      return (<div></div>)
+    }
+  }
 
   return (
-    <div className='login-container'>
-    <div className='login-card'>
-        <h3>Please login to chat!</h3>
-        <form className='loginform'> 
-            <input type="text" placeholder="name" onChange={(e)=> {setTempname(e.target.value)}}></input>
-            <br></br>
-            <input type="password" placeholder="password" onChange={(e)=> {setTempass(e.target.value)}} ></input>
-            <br></br>
-            <input type="text" placeholder="email" onChange={(e)=> {setTempemail(e.target.value)}} ></input>
-            <br></br>
-            <input type="number" placeholder="phone-number" onChange={(e)=> {setTempnumber(e.target.value)}} ></input>
-        </form>
-        <Button variant="contained" color="success" onClick={setLogindata}>login</Button>
-    </div>
-      <Footer/>
+    <div>
+      {(signup === "false") ? (<div>
+        <div className='login-container'>
+          <div className='login-card'>
+            <h3>Please login to chat!</h3>
+            <form className='loginform'>
+              <input type="text" placeholder="email" onChange={(e) => { setTempemail(e.target.value) }} ></input>
+              <br></br>
+              <input type="password" placeholder="password" onChange={(e) => { setTemppass(e.target.value) }} ></input>
+            </form>
+            <Button variant="contained" color="success" onClick={setLogindata}>login</Button>  <Button variant="contained" color="success" onClick={() => setsignup("true")}>Signup</Button>
+            <div> {setalert()} </div>
+          </div>
+          <Footer />
+        </div>
+      </div>) : (<Register />)
+      }
     </div>
   )
 }
